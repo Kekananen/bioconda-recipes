622d1801aff0d23e9c2225b3cf3b6531eaeabf4a
Kathryn Kananen <kekananen@gmail.com>
2026-02-20 13:34:04 -0500

    added in all recipe depend~

--- recipes/phylogenize/build.sh
+++ recipes/phylogenize/build.sh
@@ -5,9 +5,7 @@ export DISABLE_AUTOBREW=1
 export R_LIBS_USER="${PREFIX}/lib/R/library"
 export R_LIBS="${PREFIX}/lib/R/library"
 
-Rscript -e "install.packages('ezknitr', repos='https://cloud.r-project.org')"
-Rscript -e "install.packages('clinUtils', repos='https://cloud.r-project.org')"
-Rscript -e "install.packages('gavinmdouglas/POMS', repos='https://cloud.r-project.org')"
+#Rscript -e "install.packages('gavinmdouglas/POMS', repos='https://cloud.r-project.org')"
 
 cd package/phylogenize
 
